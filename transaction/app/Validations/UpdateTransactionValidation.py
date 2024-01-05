7dbc6a67564bc713358123511596f5eb330200e1
Harun1804 <Harun.arrasyid1804@gmail.com>
2024-01-05 14:54:54 +0700

    Fix Multiple Validate

--- transaction/app/Validations/UpdateTransactionValidation.py
+++ transaction/app/Validations/UpdateTransactionValidation.py
@@ -1,4 +1,4 @@
 from marshmallow import Schema, fields, validate
 
 class UpdateTransactionValidation(Schema):
-  status = fields.Str(required=True, validate=validate.Length(min=1), validate=validate.OneOf(["borrowed", "returning"]))
+  status = fields.Str(required=True, validate=[validate.Length(min=1), validate.OneOf(["borrowed", "returning"])])
